a194bbb1996b0c65871298a64b611bdfe537a955
dcupolillo <dario.cupolillo@gmail.com>
2025-12-17 16:01:43 +0100

    Refactor code structure to introduce a package-level folder

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-# This file is only present for backwards compatibility with older pip versions.
-# Modern pip and setuptools use pyproject.toml for configuration.
-# Run: pip install -e . to install the package.
-
 from setuptools import setup
 
 setup()
+
